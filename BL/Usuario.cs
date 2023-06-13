5b9bd551f714472d6c77b78daa81863e8b732c24
VHernandez0 <v.hernandez.r00@gmail.com>
2023-06-13 10:01:50 -0600

    Password

--- BL/Usuario.cs
+++ BL/Usuario.cs
@@ -1,30 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 
 namespace BL
 {
     public class Usuario
     {
-        public static ML.Result GetUserName(string UserName)
+        public static ML.Result GetUserName(ML.Usuario usuario)
         {
 			ML.Result result = new ML.Result(); 
 			try
 			{
 				using(DL.CinesContext context=new DL.CinesContext())
 				{
-					var query = context.Usuarios.FromSqlRaw($" GetUserName '{UserName}'").AsEnumerable().FirstOrDefault();
+					var query = context.Usuarios.FromSqlRaw($" GetUserName '{usuario.UserName}'").AsEnumerable().FirstOrDefault();
 					if (query!=null)
 					{
-						ML.Usuario usuario = new ML.Usuario();
+					
 						usuario.IdUsuario = query.IdUsuario;
 						usuario.Nombre = query.Nombre;
 						usuario.ApellidoPaterno = query.ApellidoPaterno;
 						usuario.ApellidoMaterno = query.ApellidoMaterno;
-						usuario.Password = query.Password;
+                        usuario.Password = query.Password;
 						usuario.UserName = query.Username;
 
 						result.Object = usuario;
@@ -43,6 +46,135 @@ namespace BL
 				result.ErrorMessage = ex.Message;
 			}
 			return result;
+        }
+        public static ML.Result GetEmail(string Correo)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.CinesContext context = new DL.CinesContext())
+                {
+                    var query = context.Usuarios.FromSqlRaw($" GetEmail '{Correo}'").AsEnumerable().FirstOrDefault();
+                    if (query != null)
+                    {
+                        ML.Usuario usuario = new ML.Usuario();
+                        usuario.IdUsuario = query.IdUsuario;
+                        usuario.Nombre = query.Nombre;
+                        usuario.ApellidoPaterno = query.ApellidoPaterno;
+                        usuario.ApellidoMaterno = query.ApellidoMaterno;
+                        usuario.Correo = query.Correo;
+                        usuario.UserName = query.Username;
+
+                        result.Object = usuario;
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+
+        public static ML.Result AddUsuario(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.CinesContext context=new DL.CinesContext())
+                {
+                    var query = context.Database.ExecuteSqlRaw($"Addusuario '{usuario.Nombre}', '{usuario.ApellidoPaterno}', '{usuario.ApellidoMaterno}', '{usuario.Correo}', '{usuario.UserName}', @Password", new SqlParameter("@Password", usuario.Password));
+                    
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            
+            return result;
+        }
+
+        public static ML.Result GetByEmail(ML.Usuario usuario)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.CinesContext context = new DL.CinesContext())
+                {
+                    var query = context.Usuarios.FromSqlRaw($" GetByEmail '{usuario.Correo}'").AsEnumerable().FirstOrDefault();
+                    if (query != null)
+                    {
+
+                        usuario.IdUsuario = query.IdUsuario;
+                        usuario.Nombre = query.Nombre;
+                        usuario.ApellidoPaterno = query.ApellidoPaterno;
+                        usuario.ApellidoMaterno = query.ApellidoMaterno;
+                        usuario.Password = query.Password;
+                        usuario.UserName = query.Username;
+
+                        result.Object = usuario;
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+            return result;
+        }
+        public static ML.Result UpdatePassword(string Correo, byte[] Password)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.CinesContext context = new DL.CinesContext())
+                {
+                    int RowsAffected = context.Database.ExecuteSqlRaw($"UpdatePassword '{Correo}'", "@Password", new SqlParameter("@Password", Password) );
+
+                    if (RowsAffected > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.ex = ex;
+            }
+
+            return result;
         }
 
     }
--- VHernandezCine/Controllers/LoginController.cs
+++ VHernandezCine/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Security.Cryptography;
 
 namespace VHernandezCine.Controllers
 {
@@ -11,28 +13,109 @@ namespace VHernandezCine.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(string UserName, byte[] Password)
+        public IActionResult Login(ML.Usuario usuario, string Password)
         {
-            ML.Result result = BL.Usuario.GetUserName(UserName);
-            if (result.Correct)
+            var passencrypt = new Rfc2898DeriveBytes(Password, new byte[0], 1000, HashAlgorithmName.SHA256);
+            var passconvertd = passencrypt.GetBytes(20);
+            ML.Result result = BL.Usuario.GetUserName(usuario);
+            if (result.Correct == false)
             {
-                ML.Usuario usuario = (ML.Usuario)result.Object;
-                if (UserName==usuario.UserName)
+                usuario.Password = passconvertd;
+                ML.Result result1 = BL.Usuario.AddUsuario(usuario);
+                if (result1.Correct)
                 {
-                    return View("views/Map/Index.cshtml");
+                    ViewBag.Message = "Se añadio con exito";
+                    return View("Modal");
                 }
                 else
                 {
-                    ViewBag.message = "Error de credenciales";
-                    return PartialView("Modal");
+                    ViewBag.Message = "Ocurrio un error con exito";
+                    return View("Modal");
                 }
-               
             }
             else
             {
-                ViewBag.Message = "error en las credenciales";
-                return PartialView("Modal");
+                
+                usuario = (ML.Usuario)result.Object;
+               
+                    return RedirectToAction("Index", "Home");
+           
             }
+            
+
+            
+        }
+        [HttpGet]
+        public IActionResult RecuperarPassword()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RecuperarPassword(string Correo)
+        {
+            ML.Result result = BL.Usuario.GetEmail(Correo);
+         
+            
+            if (result.Correct)
+            {
+                string EmailOrigen = "v.hernandez.r00@gmail.com";
+                MailMessage mailMessage = new MailMessage(EmailOrigen, Correo, "Recuperar Contraseña", "<p>Correo para recuperar contraseña</p>");
+                mailMessage.IsBodyHtml = true;
+                string ContenidoHTML = System.IO.File.ReadAllText(@"C:\Users\digis\Documents\Victor Hernandez Reyes\VHernandezCine\VHernandezCine\Views\Login\Correo.cshtml");
+                mailMessage.Body = ContenidoHTML+Correo;
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Port = 587;
+                smtpClient.Credentials = new System.Net.NetworkCredential(EmailOrigen, "pkslccmhnjgfxvmd");
+               
+                smtpClient.Send(mailMessage);
+                smtpClient.Dispose();
+
+               
+                ViewBag.Message = "Se ha enviado un correo de confirmación a tu correo electronico";
+                return View("Modal");
+            }
+            else
+            {
+                ViewBag.Message = "No existe el Correo Ingresado Verifique correctamente";
+                return View("Modal");
+            }
+         
+
+        }
+        [HttpGet]
+        public IActionResult NewPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewPassword(string Password1,string Password2, string UserName)
+        {
+            if (Password1==Password2)
+            {
+                var passencrypt = new Rfc2898DeriveBytes(Password1, new byte[0], 1000, HashAlgorithmName.SHA256);
+                var passconvertd = passencrypt.GetBytes(20);
+
+                ML.Result result = BL.Usuario.UpdatePassword(UserName, passconvertd);
+                if (result.Correct)
+                {
+                    ViewBag.Message = "Se ha actualizado con exito";
+                    return View("Modal");
+                }
+                else
+                {
+                    ViewBag.Message = "Ha ocurrido un error con exito";
+                    return View("ModalPassword");
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Las contraseñas no son iguales";
+                return View("ModalPassword");
+            }
+            
         }
     }
 }
